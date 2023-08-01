b202a33e4162f3bcca0148c2a644f09e7a3915ac
Amjad Khaliliah <amjadkhaliliah1998@gmail.com>
2023-08-01 23:15:07 +0300

    Initial commit

---
+++ src/main/java/com/DataExtractor/SparkyFinance/SparkyFinance/SparkyFinanceApplication.java
@@ -0,0 +1,13 @@
+package com.DataExtractor.SparkyFinance.SparkyFinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkyFinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkyFinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DataExtractor/SparkyFinance/SparkyFinance/SparkyFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DataExtractor.SparkyFinance.SparkyFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkyFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
